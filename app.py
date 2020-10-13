28d71f53cd11c05722873684adc6843b17e37b1c
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-13 19:36:12 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import time
+import random
+
+def gen_row(now: float) -> str:
+	return '{}, {}'.format(now, ", ".join([str(random.randint(1, 9)) for _ in range(4)]))
+
+if __name__ == '__main__':
+	now = time.time()
+	with open('appending.csv', 'a') as file:
+		file.write('\n' + gen_row(now))
+
+	new_filename = f'{now}.csv'
+	with open(new_filename, 'w') as file:
+		file.write(gen_row(now))
+
+	print(new_filename)
