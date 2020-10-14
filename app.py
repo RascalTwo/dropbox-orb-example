465dff51a827deaf886e164bdd62a4a7d12386b5
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-13 20:56:25 -0400

    Increase output

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import time
 import random
 
@@ -7,8 +8,16 @@ def gen_row(now: float) -> str:
 	return '{}, {}'.format(now, ", ".join([str(random.randint(1, 9)) for _ in range(4)]))
 
 if __name__ == '__main__':
+	if os.path.exists('appending.csv'):
+		print('Found file, appending')
+	else:
+		print('File not found, creating new')
+	with open('./appending.csv', 'w') as file:
+		file.write('When, #1, #2, #3, #4')
+
+
 	now = time.time()
-	with open('appending.csv', 'a') as file:
+	with open('./appending.csv', 'a') as file:
 		file.write('\n' + gen_row(now))
 
 	new_filename = f'{now}.csv'
