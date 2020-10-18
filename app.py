5dd2d2b6493729346469a91ed487b5952e97d319
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-18 15:10:55 -0400

    Simplify app.py

--- app.py
+++ app.py
@@ -12,16 +12,10 @@ if __name__ == '__main__':
 		print('Found file, appending')
 	else:
 		print('File not found, creating new')
-	with open('./appending.csv', 'w') as file:
-		file.write('When, #1, #2, #3, #4')
+		with open('./appending.csv', 'w') as file:
+			file.write('When, #1, #2, #3, #4')
 
 
 	now = time.time()
 	with open('./appending.csv', 'a') as file:
 		file.write('\n' + gen_row(now))
-
-	new_filename = f'{now}.csv'
-	with open(new_filename, 'w') as file:
-		file.write(gen_row(now))
-
-	print(new_filename)
